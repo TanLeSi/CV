68a3387354ba9eef325f1698e798ce2c87a085ae
TanLeSi <tan.lesi@study.hs-duesseldorf.de>
2022-12-31 00:35:31 +0100

    add features to view_movement

--- pages/view_movement.py
+++ pages/view_movement.py
@@ -1,20 +1,19 @@
 import streamlit as st
 import pandas as pd
-import base64, json
+import base64, json, math
 from pathlib import Path
 from utils import create_AgGrid
-from functions.inbound import inbound
-from functions.inbound.article import Article
-from st_aggrid import AgGrid, GridOptionsBuilder, JsCode, GridUpdateMode
+from st_aggrid import AgGrid, GridOptionsBuilder, JsCode, GridUpdateMode, ColumnsAutoSizeMode
 import plotly.express as px
-
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 CURRENT_DIR = Path.cwd()
 IMAGE_FOLDER = CURRENT_DIR / 'assets' /'images'
 CSS_FOLDER = CURRENT_DIR / 'styling'
 DATA_SOURCE = CURRENT_DIR / 'assets' / 'data_source'
 COUNTRY_LIST = ['AU', 'CA', 'DE', 'ES', 'FR', 'IT', 'JP', 'UK', 'USA']
-MOI = 10
+VIEW_OPTION =  ["View based on MOI","View based on markets","View based on article_no"]
 
 st.markdown('<p style="text-align:center;font-size:40px;font-weight: bold">View movement </p>', unsafe_allow_html= True)
 
@@ -88,7 +87,6 @@ def mod_stock(stock_planner: pd.DataFrame):
     stock_planner['selling_price'] = stock_planner['selling_price'].round(2)
     return stock_planner
 
-@st.cache
 def get_stock_overview():
     result = pd.read_csv(DATA_SOURCE/'AMZ_INV_all_markets.csv')
     return mod_stock(result)
@@ -102,7 +100,7 @@ def handle_operator(operator: str, MOI):
         result['between'] = f" >= {MOI[0]} && {MOI[1]} >= "
     return result[operator]
 
-def prepare_data(df: pd.DataFrame, df_MOI: pd.DataFrame):
+def prepare_data_MOI(df: pd.DataFrame, df_MOI: pd.DataFrame):
     df_group = df.groupby(by='article_no', as_index= False).agg({
         'country':'count', 
     }).rename(columns={'country': 'available_in'})
@@ -120,13 +118,22 @@ def prepare_data(df: pd.DataFrame, df_MOI: pd.DataFrame):
     df_group['pivot_data'] = df_group['pivot_data'].apply(lambda x: pd.json_normalize(json.loads(x)))
     return df_group
 
-def create_stacked_AgGrid(df, operator, MOI_threshold):
+def prepare_data_market(df: pd.DataFrame, market: str):
+    df_market = df[df['country'] == market]
+    def get_info(input: pd.Series, ori_df: pd.DataFrame):
+        return ori_df[(ori_df.article_no == input.article_no) & (ori_df.country != market)].to_json(orient="records")
+    df_market['pivot_data'] = df_market.apply(get_info, ori_df= df, axis = 1)
+    df_market['pivot_data'] = df_market['pivot_data'].apply(lambda x: pd.json_normalize(json.loads(x)))
+    return df_market
+
+def create_stacked_AgGrid_MOI(df, operator, MOI_threshold):
     gridOptions = {
         # enable Master / Detail
         "masterDetail": True,
         "rowSelection": "single",
         "pagination": True,
         "paginationPageSize": 20,
+        "preSelectedRows": [0],
         # the first Column is configured to use agGroupCellRenderer
         "columnDefs": [
             {
@@ -183,7 +190,9 @@ def create_stacked_AgGrid(df, operator, MOI_threshold):
     }
 
     custom_css = {
-        ".ag-cell-value":{"font-size":"20px !important"}
+        ".ag-cell-value":{"font-size":"18px !important"},
+        ".ag-header-cell-text":{"color":"red", "font-weight": "bold"},
+        ".ag-header-cell":{"background-color":"white"},
     }
 
     grid_table = AgGrid(
@@ -196,6 +205,132 @@ def create_stacked_AgGrid(df, operator, MOI_threshold):
     )
     sel_row = grid_table['selected_rows']
     return grid_table, sel_row
+
+def create_stacked_AgGrid_markets(df: pd.DataFrame, market: str):
+    gridOptions = {
+        # enable Master / Detail
+        "masterDetail": True,
+        "rowSelection": "single",
+        "pagination": True,
+        "paginationPageSize": 20,
+        "preSelectedRows": [0],
+        # the first Column is configured to use agGroupCellRenderer
+        "columnDefs": [
+            {
+                "field": "article_no",
+                "cellRenderer": "agGroupCellRenderer",
+                "checkboxSelection": True,
+            },
+            {"field": "selling_price"},
+            {"field": "inventory_quantity"},
+            {"field": "w4"},
+            {"field": "w3"},
+            {"field": "w2"},
+            {"field": "w1"},
+            {"field": "4_weeks_sales"},
+            {"field": "MOI"},
+            {"field": "country"},
+        ],
+        "defaultColDef": {
+            "filter": True,
+            "sortable": True,
+        },
+        # provide Detail Cell Renderer Params
+        "detailCellRendererParams": {
+            # provide the Grid Options to use on the Detail Grid
+            "detailGridOptions": {
+                "pagination": False,
+                "columnDefs": [
+                    {"field": "article_no", "checkboxSelection": True},
+                    {"field": "selling_price"},
+                    {"field": "inventory_quantity"},
+                    {"field": "w4"},
+                    {"field": "w3"},
+                    {"field": "w2"},
+                    {"field": "w1"},
+                    {"field": "4_weeks_sales"},
+                    {"field": "country"},
+                ],
+                "defaultColDef": {
+                    "sortable": True,
+                    "flex": 1,
+                },
+            },
+            # get the rows for each Detail Grid
+            "getDetailRowData": JsCode(
+                """function (params) {
+                    params.successCallback(params.data.pivot_data);
+                }"""
+            ).js_code,
+        },
+    }
+
+    custom_css = {
+        ".ag-cell-value":{"font-size":"18px !important"},
+        ".ag-header-cell-text":{"color":"red", "font-weight": "bold"},
+        ".ag-header-cell":{"background-color":"white"},
+    }
+
+    grid_table = AgGrid(
+        df,
+        gridOptions=gridOptions,
+        height=500,
+        custom_css= custom_css,
+        allow_unsafe_jscode=True,
+        columns_auto_size_mode= ColumnsAutoSizeMode.FIT_CONTENTS
+    )
+    sel_row = grid_table['selected_rows']
+    return grid_table, sel_row
+
+def create_subplot(df: pd.DataFrame, col: int):
+        rows = math.ceil(df.shape[0] / col) + 1
+        titles = ['All markets']
+        titles.extend(df['country'].unique())
+        fig_subplot = make_subplots(rows= rows, cols= col, subplot_titles= titles, vertical_spacing= round(1/(rows-1),1)/3)
+        fig_subplot.add_trace(
+            go.Bar(
+                x= df['country'],
+                y= df['4_weeks_sales'],
+                name= "all markets",
+                showlegend= False,
+                text= list(df['4_weeks_sales']),
+                textposition= "outside",
+            )
+        )
+        fig_subplot.layout[f"xaxis"].update(title="4 weeks sales by markets")
+        fig_subplot.layout[f"yaxis"].update(range=[0, max(df['4_weeks_sales']) + 20])
+        row_counter = 1
+        for index, row in df.iterrows():
+            if (index + 1) % 3 == 0:
+                row_counter += 1
+            col_counter =  1 + (index + 1) % 3  
+            fig_subplot.add_trace(
+                go.Scatter(
+                    x= ['w4','w3','w2','w1'],
+                    y=row[['w4','w3','w2','w1']],
+                    name= row.country,
+                    mode="lines+markers+text",
+                    text= list(row[['w4','w3','w2','w1']]),
+                    textposition= "top center",
+                    showlegend= False
+                ),
+                row= row_counter,
+                col= col_counter  
+            )
+            fig_subplot.layout[f"xaxis{index+2}"].update(title="Last n weeks sales")
+            fig_subplot.layout[f"yaxis{index+1}"].update(title="Pieces")
+        fig_subplot.update_layout(
+            height= rows * 400,
+            font= dict(
+                size=14,
+                color='yellow'
+            ),
+        )
+        fig_subplot.update_xaxes(showgrid= False)
+        fig_subplot.update_yaxes(showgrid= False)
+        return fig_subplot
+
+
 # def test_int_input(input):
 #     try:
 #         input = int(input)
@@ -216,14 +351,16 @@ def create_stacked_AgGrid(df, operator, MOI_threshold):
 
 
 st.header('Stock overview')
-view_MOI = st.sidebar.checkbox(label="View based on MOI", value= True, key= "MOI")
-view_markets = st.sidebar.checkbox(label="View based on markets", key= "markets")
-if view_MOI:
-    with st.expander('Explanation for using comparison'):
-        st.write(r"""
-            Stock overview shows articles that have MOI greater/less or between given MOI.
-        """)
-    stock_overview = get_stock_overview()
+stock_overview = get_stock_overview()
+
+view_mode = st.sidebar.radio(label="Choose view mode", options= VIEW_OPTION)
+if view_mode == "View based on MOI":
+    with st.expander('Explanation for view based on MOI'):
+        st.markdown(f"""
+            Stock overview shows articles that have **MOI** greater/less or between given **MOI**. <br>
+            **MOI** stands for **Months of Inventory**, which is an indicator for how much inventory there still is a warehouse. <br>
+            **MOI** is calculated by dividing the current total inventory by the sales of last 4 weeks from today.
+        """, unsafe_allow_html= True)
     overview_operator = st.radio(
         label= 'Choose operator',
         options= ('greater than', 'less than', 'between')
@@ -242,96 +379,53 @@ if view_MOI:
             stock_overview_MOI = stock_overview[stock_overview.MOI<MOI_input]
 
     st.write("Choose an article to view it across all markets")
-    stock_overview_group = prepare_data(df= stock_overview, df_MOI= stock_overview_MOI)
-    df_return, selected_row = create_stacked_AgGrid(stock_overview_group,operator= overview_operator ,MOI_threshold=MOI_input)
+    stock_overview_group = prepare_data_MOI(df= stock_overview, df_MOI= stock_overview_MOI)
+    df_return, selected_row = create_stacked_AgGrid_MOI(stock_overview_group,operator= overview_operator ,MOI_threshold=MOI_input)
     selected_df = pd.json_normalize(selected_row[0]['pivot_data'])
-    fig = px.bar(
-        x= selected_df['country'],
-        y= selected_df['4_weeks_sales']
-    )
-    st.plotly_chart(fig, use_container_width= True)
-
-
-#     st.download_button(
-#             label= f'Download overview MOI_{MOI_input}',
-#             data = pd.DataFrame.from_dict(df_return['data']).to_csv(index= False).encode('utf-8'),
-#             file_name= f'overview_MOI_{MOI_input}.csv',
-#             mime='csv'
-#         )
-#     if selected_row_std[0]:
-#         selected_article = selected_row_std[0]['article_no']
-#         create_download_button(df= get_MOI_article(selected_article),
-#                             download_header= f"Download {selected_article} across all markets",
-#                             file_name= f"{selected_article} across all markets",
-#                             button_key= "MOI_article")
+    st.download_button(
+            label= f'Download MOI_{overview_operator}_{MOI_input}',
+            data = stock_overview[stock_overview.article_no.isin(df_return.data.article_no.values)].to_csv(index= False).encode('utf-8'),
+            file_name= f'MOI_{overview_operator}_{MOI_input}.csv',
+            mime='csv'
+        )
+    st.plotly_chart(create_subplot(df= selected_df, col= 3), use_container_width= True)
                             
-# if view_markets:
-#     view_type = st.radio(
-#             label= 'Choose view type:',
-#             options= ('by market', 'by article_no')
-#         )
+if view_mode == VIEW_OPTION[1]:
+    st.info(f"""
+        Enter market to view MOI of all articles in that market.""")
+    view_type = st.radio(
+            label= 'Choose view type:',
+            options= ('by market', 'by article_no')
+        )
 
-#     with st.expander('Explanation for each view type'):
-#         st.write(r"""
-#             This function provides two ways of viewing MOI of different articles. 
-#             1. View by market:\
-#                 Enter market to view MOI of all articles in that market. The articles are divide into 4 Category: 
-#                 * STD, NEW, CEZ and EOL
-#             2. View by article_no:\
-#                 Enter article_no to view MOI of te given article_no across all markets.
-#         """)
+    if view_type == 'by market':
+        st.write(f"available markets: {', '.join(COUNTRY_LIST)} or type ALL to view all markets")
+        country_code = st.text_input("Please type market in", 'DE')
+        if country_code.lower() == 'all':
+            st.download_button(
+                label= f'Download all markets',
+                data = stock_overview.to_csv(index= False).encode('utf-8'),
+                file_name= f'all_markets.csv',
+                mime='csv'
+            )
+            st.stop()
+        if country_code not in COUNTRY_LIST:
+            st.warning(f'{country_code} is not acceptable as a market')
+            st.stop()
+        stock_overview = prepare_data_market(stock_overview, market= country_code)
+        df_return, selected_row = create_stacked_AgGrid_markets(stock_overview, market=country_code)
 
-#     if view_type == 'by market':
-#         st.write(f"available markets: {', '.join(COUNTRY_LIST)} or type ALL to view all markets")
-#         country_code = st.text_input("Please type market/partnered warehouse in", 'ES')
-#         if country_code.lower() == 'all':
-#             stock_planner_all = pd.DataFrame()
-#             for each_country in COUNTRY_LIST:
-#                 temp = get_stock_planner(country_code= each_country)
-#                 stock_planner_all = pd.concat([stock_planner_all, temp], ignore_index= True)
-#             create_download_button(df=stock_planner_all,
-#                             download_header=f"Download Amazon Inventory for all markets",
-#                             file_name=f"AMZ_INV_all_markets",
-#                             button_key=f"AMZ_INV_all")
-#             st.stop()
-#         if country_code not in COUNTRY_LIST:
-#             st.warning(f'{country_code} is not acceptable as a market')
-#             st.stop()
-#         stock_planner = get_stock_planner(country_code= country_code)
-
-#         # for STD products
-#         create_download_button(df=stock_planner[(stock_planner['status'] == 'STD') & (~stock_planner['factory'].isin(['CEZ']))].sort_values(by= 'MOI', ascending= False).reset_index(drop= True),
-#                                 download_header=f"Download STD {country_code}",
-#                                 file_name=f"STD_of_{country_code}",
-#                                 button_key=f"std_{country_code}")
-
-
-#         # for NEW products
-#         create_download_button(df=stock_planner[(stock_planner['status'].isin(['NEW', 'READY'])) & (~stock_planner['factory'].isin(['CEZ']))].sort_values(by= 'MOI', ascending= False).reset_index(drop= True),
-#                                 download_header=f"Download NEW {country_code}",
-#                                 file_name=f"NEW_of_{country_code}",
-#                                 button_key=f"new_{country_code}")
-
-
-#         # for CEZ products
-#         create_download_button(df= stock_planner[stock_planner['factory'].isin(['CEZ'])].sort_values(by= 'MOI', ascending= False).reset_index(drop= True),
-#                                 download_header=f"Download CEZ {country_code}",
-#                                 file_name=f"CEZ_of_{country_code}",
-#                                 button_key=f"cez_{country_code}")
-
-#         # for EOL products
-#         create_download_button(df= stock_planner[stock_planner['status'] == 'EOL'].sort_values(by= 'MOI', ascending= False).reset_index(drop= True),
-#                                 download_header=f"Download EOL {country_code}",
-#                                 file_name=f"EOL_of_{country_code}",
-#                                 button_key=f"eol_{country_code}")
-
-#     elif view_type == 'by article_no':
-#         article_no_input = st.text_input("Please type article_no in", '11525')
-#         # try:
-#         #     article_no_input = int(article_no_input)
-#         # except:
-#         #     st.write("Article_no must be a number!")
-#         #     st.stop()
+if view_mode == VIEW_OPTION[2]:
+    st.info(f"""
+        Enter article_no to view MOI of that article across all markets.""")
+    article_no_input = st.text_input("Please type article_no in", '11525, 10844, 11635')
+    article_no_input = article_no_input.replace(' ','').split(',')
+    try:
+        article_no_input = list(map(int, article_no_input))
+    except:
+        st.error("Article_no must be a number!")
+        st.stop()
+    a, b = create_AgGrid(df= stock_overview[stock_overview.article_no.isin(article_no_input)].sort_values('article_no'), selection_mode= False)
 #         create_download_button(df= get_MOI_article(article_nos=article_no_input),
 #                             download_header= f"Download {article_no_input} across all markets",
 #                             file_name= f"{article_no_input} across all markets",
--- utils.py
+++ utils.py
@@ -3,6 +3,11 @@ from st_aggrid import AgGrid, GridUpdateMode, ColumnsAutoSizeMode, JsCode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 def create_AgGrid(df, button_key= 0, selection_mode= True):
+    custom_css = {
+        ".ag-cell-value":{"font-size":"18px !important"},
+        ".ag-header-cell-text":{"color":"red", "font-weight": "bold"},
+        ".ag-header-cell":{"background-color":"white", "opacity": 1},
+    }
     gd = GridOptionsBuilder.from_dataframe(df)
     gd.configure_pagination(enabled= True, paginationAutoPageSize= False, paginationPageSize= 20)
     if selection_mode:
@@ -18,8 +23,8 @@ def create_AgGrid(df, button_key= 0, selection_mode= True):
                         fit_columns_on_grid_load= True,
                         # key= button_key,
                         reload_data= True,
-                        columns_auto_size_mode= ColumnsAutoSizeMode.FIT_CONTENTS
-                        )
+                        columns_auto_size_mode= ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW,
+                        custom_css= custom_css)
     sel_row = grid_table['selected_rows']
     return grid_table, sel_row
 
