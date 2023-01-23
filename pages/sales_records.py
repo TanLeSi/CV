def1729734758f03fa04fb04230a4c9da338008c
TanLeSi <tan.lesi@study.hs-duesseldorf.de>
2023-01-23 13:23:07 +0100

    add sidebar

--- pages/sales_records.py
+++ pages/sales_records.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np  
 import json, sys
 from pathlib import Path
 from utils import create_AgGrid
@@ -18,22 +19,19 @@ st.markdown('<p style="text-align:center;font-size:40px;font-weight: bold">Sales
 with open(CSS_FOLDER/'main.css') as css_file:
     st.markdown(f"<style>{css_file.read()}</style>", unsafe_allow_html=True)
 
-# sales_record = pd.read_excel(DATA_SOURCE / 'sales_records.xlsx')
 @st.experimental_memo
 def get_sales_record():
-    country_sales = {each_country: pd.read_excel(DATA_SOURCE / 'sales_records.xlsx', sheet_name=each_country) for each_country in COUNTRY_LIST}
-    return country_sales
+    # country_sales = {each_country: pd.read_excel(DATA_SOURCE / 'sales_records.xlsx', sheet_name=each_country) for each_country in COUNTRY_LIST}
+    # return country_sales
+    country_sales = pd.read_excel(DATA_SOURCE / 'sales_records.xlsx', sheet_name= None)
+    return pd.concat(country_sales.values())
 
-country_sales = get_sales_record()
-
-
-
-selected_product = st.text_input('**Enter an article number**',placeholder= "e.g. 11525, 10844, 11167, ...", key='product_input')
 @st.experimental_memo
-def display_chart(article_no: int):
-    for each_country in COUNTRY_LIST:
-        sales_report = country_sales[each_country]
-        sales_report = sales_report[sales_report['article_no']==article_no]
+def display_chart(df: pd.DataFrame, country_list: list[str]):
+    if 'All' in country_list:
+        country_list = COUNTRY_LIST
+    for each_country in country_list:
+        sales_report = df[df['country']==each_country]
         if sales_report.shape[0]:
             fig = go.Figure()
 
@@ -76,7 +74,18 @@ def display_chart(article_no: int):
             )
             st.plotly_chart(fig, use_container_width=True)
 
-if st.session_state['product_input']:
-    display_chart(article_no= int(selected_product))
-else:
+country_sales = get_sales_record()
+
+
+selected_product = st.text_input('**Enter an article number**',placeholder= "e.g. 11525, 10844, 11167, ...", key='product_input')
+try:
+    selected_product = int(selected_product)
+except:
+    st.info("Article number must be a valid number")
     st.stop()
+
+sales_report = country_sales[country_sales['article_no'] == selected_product]
+selected_markets = st.sidebar.multiselect(label= "Choose marketto view", options= np.append('All',sales_report.country.unique()), default='All')
+
+if st.session_state['product_input']:
+    display_chart(df= sales_report, country_list= selected_markets)
